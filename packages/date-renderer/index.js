007454240eb8916c7abd1760e070fc4a836c4b76
netosha <antoha-surgut@yandex.ru>
2022-05-19 21:03:25 +0300

    test commit 3

--- packages/date-renderer/index.js
+++ packages/date-renderer/index.js
@@ -2,6 +2,7 @@ const getDate = require('@xcanchal/date-logic');
 
 (() => {
   console.log('test')
-  console.log('test 2')
+  console.log('test 24')
+  console.log('test 3')
   console.log(`Date: ${getDate()}`);
 })();
